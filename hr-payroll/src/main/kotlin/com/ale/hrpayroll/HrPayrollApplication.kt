f4e4741f6374782eeb724518ca05615d054d03d6
Ale <alexandre.corda@lab4e.com.br>
2020-12-12 20:21:50 -0300

    api gateway instanciado

--- hr-payroll/src/main/kotlin/com/ale/hrpayroll/HrPayrollApplication.kt
+++ hr-payroll/src/main/kotlin/com/ale/hrpayroll/HrPayrollApplication.kt
@@ -2,12 +2,14 @@ package com.ale.hrpayroll
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
+@EnableCircuitBreaker
 class HrPayrollApplication
 
 fun main(args: Array<String>) {
--- hr-payroll/src/main/kotlin/com/ale/hrpayroll/resource/PaymentResource.kt
+++ hr-payroll/src/main/kotlin/com/ale/hrpayroll/resource/PaymentResource.kt
@@ -2,6 +2,7 @@ package com.ale.hrpayroll.resource
 
 import com.ale.hrpayroll.domain.Payment
 import com.ale.hrpayroll.service.PaymentService
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/payments")
 class PaymentResource @Autowired constructor(private val service: PaymentService) {
+
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     fun getPayment(@PathVariable workerId: Long, @PathVariable days: Int): ResponseEntity<Payment> {
         val payment = service.getPayment(workerId, days)
@@ -20,5 +23,10 @@ class PaymentResource @Autowired constructor(private val service: PaymentService
         } else {
             ResponseEntity.notFound().build()
         }
+    }
+
+    fun getPaymentAlternative(workerId: Long, days: Int): ResponseEntity<Payment> {
+        val payment = Payment("Brann", 400.0, days)
+        return ResponseEntity.ok(payment)
     }
 }
